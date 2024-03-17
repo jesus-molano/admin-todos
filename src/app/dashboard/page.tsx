eae0526c80aaae3ad01ff5e3f8ad146eb70cdfbe
jesus-molano <jessumolano@gmail.com>
2024-03-17 15:53:05 +0000

    Created Github signin with next auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,22 @@
-export default function DashboardPage() {
-  return <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3"></div>;
+import { getServerSession } from "next-auth";
+import { WidgetItem } from "../../components/WidgetItem/index";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+  return (
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      <WidgetItem title="Connected User (Server Side)">
+        <div className="flex flex-col">
+          <span>{session?.user?.name}</span>
+          <span>{session?.user?.email}</span>
+        </div>
+      </WidgetItem>
+    </div>
+  );
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -10,6 +10,8 @@ import {
   IoListOutline,
   IoSaveOutline,
 } from "react-icons/io5";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const SIDEBAR_ITEMS = [
   {
@@ -39,7 +41,16 @@ const SIDEBAR_ITEMS = [
   },
 ];
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+  const user = {
+    name: session?.user?.name ?? "John Doe",
+    // role: session?.user?.role ?? 'User',
+    avatarUrl:
+      session?.user?.image ??
+      "https://tailus.io/sources/blocks/stats-cards/preview/images/first_user.webp",
+  };
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -57,14 +68,14 @@ export const Sidebar = () => {
 
         <div className="mt-8 text-center">
           <Image
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
+            src={user.avatarUrl}
             alt=""
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
             width={112}
             height={112}
           />
           <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            Cynthia J. Watts
+            {user.name}
           </h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
